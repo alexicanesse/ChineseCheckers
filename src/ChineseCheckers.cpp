9788e06efd1fb3ebe5029ae852ef0cf9dcd6f5c2
alexicanesse <alexi.canesse@protonmail.ch>
2022-10-06 13:04:42 +0100

    Initial commit

---
+++ src/ChineseCheckers.cpp
@@ -0,0 +1,35 @@
+//
+//  ChineseCheckers.cpp
+//  Chinese Checkers
+//
+//  Created on 06/10/2022.
+//
+
+#include "../include/ChineseCheckers.hpp"
+
+#include <vector>
+
+MoveType ChineseCheckers::elementaryMove(PositionType original_position, PositionType arrival_position){
+#warning TODO
+    return Illegal; //This is just to remove the error while this function is not implemented
+};
+
+ChineseCheckers::ChineseCheckers(){
+#warning TODO
+};
+
+bool ChineseCheckers::move(Player player, std::vector<PositionType> list_moves){
+#warning TODO
+    return true; //This is just to remove the error while this function is not implemented
+}
+
+bool ChineseCheckers::is_finished(){ /* returns true or false to indicate if the current position is a winning position */
+#warning TODO
+    return true; //This is just to remove the error while this function is not implemented
+}
+
+void ChineseCheckers::new_game(int nbr_players, int nbr_colors){
+#warning TODO
+}
+
+
