b7ff15c17248e95ad1e3955842e842149f12589d
Clément Yvernes <101457597+Cyvernes@users.noreply.github.com>
2023-01-28 18:29:57 +0100

    basic mutation and random generators

--- src/tournament.cpp
+++ src/tournament.cpp
@@ -17,6 +17,8 @@
 
 /* C++ libraries */
 #include <vector>
+#include <random>
+#include <chrono>
 /* The following pragma are used to removed deprecation warning from boost
  * header files. Using them avoid to remove this warning from the entire project.
  */
@@ -31,13 +33,14 @@
 #include "AlphaBeta.hpp"
 
 Result playGame(AlphaBeta &player0, AlphaBeta &player1, const int &depth) {
-    while(player0.state_of_game() == NotFinished) {
+    int remaining_moves = 100;
+    while((player0.state_of_game() == NotFinished) && (remaining_moves > 0)) {
         auto move = player0.getMove(depth, -100000, 100000);
         player0.move(0, move);
         player1.move(0, move);
 
-        player0.print_grid_();
-        std::cout << "\n";
+        //player0.print_grid_();
+        //std::cout << "\n";
 
         if(player0.state_of_game() != NotFinished)
             return player0.state_of_game();
@@ -46,10 +49,10 @@ Result playGame(AlphaBeta &player0, AlphaBeta &player1, const int &depth) {
         player0.move(1, move);
         player1.move(1, move);
 
-        player0.print_grid_();
-        std::cout << "\n";
+        //player0.print_grid_();
+        //std::cout << "\n";
+        --remaining_moves;
     }
-
     return player0.state_of_game();
 }
 
@@ -61,8 +64,44 @@ void print_matrix(const std::vector< std::vector<double> > &matrix) {
     }
 }
 
+//Evolution parameters
+double p_mutation = 0.05; /*Probability of mutation of an element*/
+double sigma_mutation = 1; /*variability of a mutation*/
+//Creating distribution generators
+unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+//std::random_device generator;
+std::mt19937 generator(seed);
+std::bernoulli_distribution b_distrib(p_mutation);
+std::normal_distribution<double> n_distrib(0,sigma_mutation);
+auto mutates = std::bind(b_distrib ,generator);
+auto variation = std::bind(n_distrib,generator);
+
+
+void mutate_matrix(std::vector< std::vector<double> > &matrix) {
+    for (int i = 0; i != 8; ++i) {
+        for (int j = 0; j != 8; ++j) {
+            if (mutates()) {
+                matrix[i][j] = matrix[i][j] + variation();
+            }
+        }
+    }
+}
+
 int main() {
-    
+    int population_size = 10;
+    std::cout << "Current seed: "<< seed << std::endl;
+    AlphaBeta best_white;
+    AlphaBeta best_black;
+    auto matrix1 = best_white.get_player_to_win_value_();
+    auto matrix2 = best_white.get_player_to_loose_value_();
+
+    std::vector< AlphaBeta > population(population_size);
+    for (int i = 0; i != population_size; ++i) {
+        AlphaBeta newplayer;
+        population[i] = newplayer;
+    }
+
+    /*
     std::vector< std::vector<double> > matrix(8, std::vector<double>(8));
     print_matrix(matrix);
 
@@ -71,7 +110,7 @@ int main() {
             if (i + j < 3) matrix[i][j] = -42*(i + j);
 
     
-    print_matrix(matrix);
+    */
     // AlphaBeta player0;
     // auto matrix = player0.get_player_to_win_value_();
     // AlphaBeta player1(matrix, matrix);
