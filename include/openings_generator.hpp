58ee6d5bdf0c0302dc0a708bd2df8d49ace60464
alexicanesse <alexi.canesse@protonmail.ch>
2023-02-10 15:24:14 +0000

    initial commit

---
+++ include/openings_generator.hpp
@@ -0,0 +1,32 @@
+/*
+ * This file is part of ChineseCheckers which is released under GNU General Public License v3.0.
+ * See file LICENSE or go to https://github.com/alexicanesse/ChineseCheckers/blob/main/LICENSE for full license details.
+ * Copyright 2022 - ENS de Lyon
+ */
+
+/**
+ * \file openings_generator.hpp
+ * \brief
+ *
+ *
+ *
+ */
+
+#ifndef CHINESECHECKERS_OPENINGS_GENERATOR_HPP
+#define CHINESECHECKERS_OPENINGS_GENERATOR_HPP
+
+/* C++ libraries */
+/* The following pragma are used to removed depraction warning from boost
+ * header files. Using them avoid to remove this warning from the entire project.
+ */
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#include <boost/python.hpp>
+#pragma GCC diagnostic pop
+
+/* Other */
+#include "Types.hpp"
+#include "AlphaBeta.hpp"
+
+
+#endif //CHINESECHECKERS_OPENINGS_GENERATOR_HPP
---
+++ src/openings_generator.cpp
@@ -0,0 +1,29 @@
+/*
+ * This file is part of ChineseCheckers which is released under GNU General Public License v3.0.
+ * See file LICENSE or go to https://github.com/alexicanesse/ChineseCheckers/blob/main/LICENSE for full license details.
+ * Copyright 2022 - ENS de Lyon
+ */
+
+/**
+ * \file openings_generator.cpp
+ * \brief
+ *
+ *
+ *
+ */
+
+/* openings_generator.hpp */
+#include "openings_generator.hpp"
+
+/* C++ libraries */
+/* The following pragma are used to removed deprecation warning from boost
+ * header files. Using them avoid to remove this warning from the entire project.
+ */
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#include <boost/python.hpp>
+#pragma GCC diagnostic pop
+
+/* Other */
+#include "Types.hpp"
+#include "AlphaBeta.hpp"
